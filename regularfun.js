170d0332aee452df26e8fb927301b338d69d00b1
SalsabilaYulianti <141704344+SalsabilaYulianti@users.noreply.github.com>
2023-10-04 15:00:12 +0700

    Add files via upload

---
+++ regularfun.js
@@ -0,0 +1,9 @@
+function login(user, pass){
+    console.log("----------------------------------")
+    console.log("             login")
+    console.log("----------------------------------")
+    console.log("username: " + user)
+    console.log("pass: " + pass)
+    console.log("-----------------------------------")
+}
+login("salsabila yulianti ananta" , 150707)
